4076680c901dab8a9144d0319ed3b6ed7e6bb618
mayank1312 <mayankdhingra1312@gmail.com>
2025-01-28 22:54:08 +0530

    index.html

--- src/App.jsx
+++ src/App.jsx
@@ -52,28 +52,28 @@ const Portfolio = () => {
   };
   return (
     <div className="container1">
-      <nav className="navbar navbar-expand-lg">
-  <div className="container-fluid" >
-    <a className="navbar-brand" href="#">Mayank dhingra</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid" >
+    <a class="navbar-brand" href="#">Mayank dhingra</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#about">About</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#about">About</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#skills">Skills</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#skills">Skills</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#projects">Projects</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">Projects</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#education">Education</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#education">Education</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#contact">Contact me</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact me</a>
         </li>
       </ul>
     </div>
